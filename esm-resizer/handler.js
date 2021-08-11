bac62b9a4048f63658b042b032e543e328022961
Aamir.R <r-aamir@users.noreply.github.com>
2021-08-11 06:30:55 +0500

    changes

--- esm-resizer/handler.js
+++ esm-resizer/handler.js
@@ -1,46 +1,76 @@
 import { readStreamFromS3, writeStreamToS3, streamToSharp } from './src/resize';
 
-const BUCKET = process.env.BUCKET;
-const URL = 'https://resizer.bocaapp.com';
+const SRC_BUCKET = 'cdn.esmart.by';
+const DST_BUCKET = process.env.BUCKET;
+const dstUrl = `http://${process.env.DST_BUCKET}.s3-website.${process.env.REGION}.amazonaws.com/`;
 
-export const resize = async (event) => {
+exports.handler = async (event) => {
+  // https://github.com/sagidM/s3-resizer/blob/master/index.js
+  // RewriteRule ^(.+)/(.+)/(.+)$ gi.php?type=$1&src=$2&file=$3 [QSA]
   const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+)x(\d+)\/(.*)/);
-  const width = parseInt(match[1], 10);
-  const height = parseInt(match[2], 10);
-  const originalKey = match[3];
-  const newKey = '' + width + 'x' + height + '/' + originalKey;
-  const imageLocation = `${URL}/${newKey}`;
+  const sharpConfig = {}; // {fit: "inside", withoutEnlargement: true}
+  const types = {p: 1, u: 1, att: 1};
+  const [type, dstDir, fileName] = key.split('/');
+  const srcPath = `${types[type]}/${fileName}`;
+  const dstPath = `${types[type]}/${dstDir}/${fileName}`;
+  
+  let [width, height, mode] = dstDir.split('x');
+
+  width = parseInt(width) || null;
+  height = parseInt(height) || null;
+  
+
+  if (mode === 'l') {
+      // ScaleAspectFill, scaled to fill. some portion of image may be clipped
+  } else {
+    if (!height) {
+      // calc scale and height
+    } else if (!width) {
+      // calc scale and width
+    } else {
+      // calc scale = min(height / originalHeight, width / originalWidth);
+    }
+
+    if (mode === 'l' || mode === 'r') { // r: ScaleAspectFit, scale to minimum
+    } else if (mode === 't') { // Transparent
+    } else if (isRGB(mode)) {
+    }
+  }
+
+//  console.log({key: key, type: type, src: src, file: file, width: width, height: height, mode: mode});
 
   try {
-    const readStream = readStreamFromS3({ Bucket: BUCKET, Key: originalKey });
-    const resizeStream = streamToSharp({ width, height });
-    const {
-      writeStream,
-      uploadFinished
-    } = writeStreamToS3({ Bucket: BUCKET, Key: newKey });
+    // source bucket read stream from S3
+    const readStream = readStreamFromS3({ Bucket: SRC_BUCKET, Key: srcPath });
 
+    // sharp resize stream
+    const resizeStream = streamToSharp({ width, height, sharpConfig });
+
+    // destination bucket write streams to S3
+    const { writeStream, uploadFinished } = writeStreamToS3({ Bucket: DST_BUCKET, Key: dstPath });
+
+    // trigger the stream
     readStream
       .pipe(resizeStream)
       .pipe(writeStream);
 
+    // wait for the stream to finish
     const uploadedData = await uploadFinished;
 
-    console.log('Data: ', {
-      ...uploadedData,
-      BucketEndpoint: URL,
-      ImageURL: imageLocation
-    });
+    // log data to Dashbird
+    // console.log('Data: ', {...uploadedData, BucketEndpoint: URL, ImageURL: url});
 
+    // return a 301 redirect to the newly created resource in S3
     return {
       statusCode: '301',
-      headers: { 'location': imageLocation },
+      headers: {location: dstUrl + dstPath},
       body: ''
-    }
+    };
   } catch (err) {
+    console.error(err);
     return {
       statusCode: '500',
       body: err.message
-    }
+    };
   }
 };
--- esm-resizer/src/resize.js
+++ esm-resizer/src/resize.js
@@ -1,33 +1,34 @@
-import AWS from 'aws-sdk';
+import awS3 from 'aws-sdk/clients/s3';
 import sharp from 'sharp';
 import stream from 'stream';
 
-const S3 = new AWS.S3({
-  signatureVersion: 'v4',
-});
+const s3 = new awS3({signatureVersion: 'v4'});
 
+// create the read stream abstraction for getting object data from S3
 const readStreamFromS3 = ({ Bucket, Key }) => {
-  return S3.getObject({ Bucket, Key }).createReadStream()
+  return s3.getObject({ Bucket, Key }).createReadStream()
 };
 
+// create the write stream abstraction for uploading data to S3
 const writeStreamToS3 = ({ Bucket, Key }) => {
   const pass = new stream.PassThrough();
   return {
     writeStream: pass,
-    uploadFinished: S3.upload({
+    uploadFinished: s3.upload({
       Body: pass,
       Bucket,
       ContentType: 'image/png',
       Key
     }).promise()
-  }
+  };
 };
 
-const streamToSharp = ({ width, height }) => {
+// sharp resize stream
+const streamToSharp = ({ w, h, config }) => {
   return sharp()
-    .resize(width, height)
-    .toFormat('png')
-};
+    .resize(w, h, config)
+    .toFormat('png') // .jpeg()
+}
 
 export {
   readStreamFromS3,
